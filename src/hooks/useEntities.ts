9b847e1216670e907e0d39451ec892d03e6db9d8
Omar Gaxiola <gaxiolaomar04@gmail.com>
2025-08-19 13:19:32 -0600

    fix 6

--- src/hooks/useEntities.ts
+++ src/hooks/useEntities.ts
@@ -1,3 +1,4 @@
+import React from 'react';
 import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query';
 import { databases, DATABASE_ID, ENTITIES_COLLECTION_ID } from '@/lib/appwrite';
 import { Entity } from '@/types';
